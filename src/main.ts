4606421406f3d81747dd003b01273ae8afd7ace0
axixe <s1ngleez@yandex.ru>
2025-07-07 11:18:58 +0300

    feat(main): create settings for vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+app.use(vuetify)
+
+
+app.mount('#app')
